42232de0b0ba22f8a2a298273064dcedcc46b463
RowinVA <rowin.amsterdam@gmail.com>
2023-10-09 14:39:11 +0200

    Revert back to buildjs

--- build.js
+++ build.js
@@ -4,9 +4,9 @@ const StyleDictionaryPackage = require('style-dictionary');
 
 StyleDictionaryPackage.registerFormat({
     name: 'css/variables',
-    formatter: function (dictionary: { allProperties: any[]; }, config: any) {
+    formatter: function (dictionary, config) {
       return `${this.selector} {
-        ${dictionary.allProperties.map((prop: { name: any; value: any; }) => `  --${prop.name}: ${prop.value};`).join('\n')}
+        ${dictionary.allProperties.map(prop => `  --${prop.name}: ${prop.value};`).join('\n')}
       }`
     }
   });  
@@ -14,17 +14,17 @@ StyleDictionaryPackage.registerFormat({
 StyleDictionaryPackage.registerTransform({
     name: 'sizes/px',
     type: 'value',
-    matcher: function(prop: { attributes: { category: string; }; }) {
+    matcher: function(prop) {
         // You can be more specific here if you only want 'em' units for font sizes    
         return ["fontSize", "spacing", "borderRadius", "borderWidth", "sizing"].includes(prop.attributes.category);
     },
-    transformer: function(prop: { original: { value: string; }; }) {
+    transformer: function(prop) {
         // You can also modify the value here if you want to convert pixels to ems
         return parseFloat(prop.original.value) + 'px';
     }
     });
 
-function getStyleDictionaryConfig(theme: string) {
+function getStyleDictionaryConfig(theme) {
   return {
     "source": [
       `tokens/${theme}.json`,
