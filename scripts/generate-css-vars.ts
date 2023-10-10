a9c0610143d35b4191be219676ba9d71205d375c
RowinVA <rowin.amsterdam@gmail.com>
2023-10-10 11:19:11 +0200

    Added script

---
+++ scripts/generate-css-vars.ts
@@ -0,0 +1,6 @@
+import StyleDictionary from "style-dictionary";
+
+StyleDictionary.buildPlatform({
+  transforms: ['attribute/cti', 'name/cti/kebab', 'size/rem'],
+  output: 'path-to-output-directory',
+});
