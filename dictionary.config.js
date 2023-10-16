7a6f7c66e3492b9df55fbf10a36f89876f12dc91
RowinVA <rowin.amsterdam@gmail.com>
2023-10-16 12:32:34 +0200

    Removed GH action, style-dictionary, output css variables files

--- dictionary.config.js
+++
@@ -1,5 +0,0 @@
-const StyleDictionary = require('style-dictionary');
-
-StyleDictionary.extend('./path-to-your-design-token-file.json');
-
-module.exports = StyleDictionary;
--- scripts/generate-css-vars.ts
+++
@@ -1,6 +0,0 @@
-import StyleDictionary from "style-dictionary";
-
-StyleDictionary.buildPlatform({
-  transforms: ['attribute/cti', 'name/cti/kebab', 'size/rem'],
-  output: 'path-to-output-directory',
-});
